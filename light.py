be151d8cde43beeb31ca86b7295284cc62ba296f
Chang-Tun-Yu <b07901098@ntu.edu.tw>
2018-12-27 21:48:36 +0800

    light (with total rerlection problem)
    Signed-off-by: Chang-Tun-Yu <b07901098@ntu.edu.tw>

--- light.py
+++ light.py
@@ -2,60 +2,87 @@ from vpython import *
 import math
 n_oil = 1.7
 n_water = 1.33
-"""
-scene = canvas(width=800,height=800,center=vec(0,0,0),background=vec(0,0.3,0.3))
-oil = box(pos=vec(0,2,0),size=vec(200,2,200),color=color.red)
-water = box(pos=vec(0,0,0),size=vec(200,2,200))
 
-"""
 class light:
-	def __init__(self,wavelength=None,v=vec(0,0,0)):
-		self.wl = wavelength
+	def __init__(self,wl,pos,v):  # wl = wavelength 
+		self.wl = wl
 		self.v = v
-		self.pos = vec(0,0,0)
+		self.pos = pos
 		self.length = 0
 		self.phase = 0
+		self.condition = 0
+
 	def refraction(self,n1,n2):
 		in_sintheta = self.v.x/mag(self.v)
 		if n1*in_sintheta>=n2:
 			self.v.y = -self.v.y
-			print("all reflection")
+			print("total reflection")
 		else:
 			out_sintheta = in_sintheta*(n1/n2)
 			new_v = self.v.mag*(n1/n2)
 			self.v = vec(new_v*math.sqrt(1-out_sintheta**2),-new_v*out_sintheta,0)
 			print("refraction")
 
+	def reflection(self):
+		self.v.y = -self.v.y
+
+def touchsensor(pos):
+	if pos.x>=5 and pos.x>=7 and pos.y>=5 and pos.y<=7 and pos.x+pos.y>12:
+		return True
+	else:
+		return False
 
 
-ball = light(400*10**(-7))
+beam = light(400*10**(-7),vec(-6,6,0),vec(6,-6,0))
 
-ball.v = vec(6,-6,0)
-ball.pos = vec(-5.999999999,5.999999999,0)
-
-dt = 0.001
-condition = 0
+dt = 0.0001
+count = 0
 
 while True:
-	rate(10000)
-	ball.pos += ball.v*dt
-	print(ball.pos)
-	print(ball.v)
-	ball.length += mag(ball.v)*dt
-	if 2<=ball.pos.y and ball.pos.y<=4 and condition == 0:
-		ball.refraction(1,n_oil)		
-		condition = 1
-		print("*"*60)
-
-	if  0<=ball.pos.y and ball.pos.y<=2 and condition==1:
+	beam.pos += beam.v*dt
+	beam.length += mag(beam.v)*dt
+	try:
+		re_beam.pos += re_beam.v*dt
+		re_beam.length += mag(re_beam.v)*dt
+	except:
+		pass
+	
+	if 2<=beam.pos.y and beam.pos.y<=4 and beam.condition == 0:
 		
-		ball.refraction(n_oil,n_water)
-		print("#"*60)
-		condition = 2
-		break
-print(ball.length)
-ball.phase = ((ball.length % ball.wl) /ball.wl) *2*pi 
-print(ball.phase)
+		beam.refraction(1,n_oil)		
+		beam.condition = 1
+		re_beam = light(beam.wl,beam.pos,vec(beam.v.x,-beam.v.y,beam.v.z))
+		
+	if  0<=beam.pos.y and beam.pos.y<=2 and beam.condition == 1:
+		
+		beam.refraction(n_oil,n_water)
+		beam.condition = 2
+		
+	
+	if  beam.pos.y>=4 and beam.condition == 2:
+		print(111)
+		beam.refraction(n_water,1)
+		beam.condition = 3
+	
+	if touchsensor(beam.pos):
+		count += 1
+	try:
+		if touchsensor(re_beam.pos):
+			count += 1
+	except:
+		pass
 
-	#if ball.pos.y<=0 and :
-		#ball.v.y = -ball.v.y
+	if count == 2:
+		break
+
+
+print(beam.length)
+beam.phase = ((beam.length % beam.wl) /beam.wl) *2*pi 
+print(beam.phase)
+print("pos",beam.pos)
+
+print(re_beam.length)
+re_beam.phase = ((re_beam.length % re_beam.wl) / re_beam.wl) *2*pi 
+print(re_beam.phase)
+
+	
