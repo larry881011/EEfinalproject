48403d0e5432f1de80d44c4045ccaabdec2d9196
myhsiehlol <b07901025@ntu.edu.tw>
2019-01-11 23:15:00 +0800

    add

--- light_0111_SumAmp.py
+++ light_0111_SumAmp.py
@@ -2,17 +2,6 @@ from vpython import *
 import math
 import numpy as np
 
-n = [1,1.5,1.33]		# refraction index (air, oil, water)
-wavelength = [450,540,680] #blue,green,red
-count = 0
-
-Xoil, Yoil, Zoil = 0.005,0,0				# 1 point which is on the air-oil plane
-thetaD = 87#degree					# normal vector of the air-oil plane
-XoilD, YoilD, ZoilD = math.cos( thetaD* 2*pi/360),math.sin( thetaD* 2*pi/360),0
-
-Xwater, Ywater, Zwater = 0.005,0,0
-XwaterD,YwaterD,ZwaterD= 0,1,0
-
 class light:
 	def __init__(self,wl,origin,direction,length=0,amplitude=1):   
 		self.wl = wl 					# wavelength 				( m )
@@ -24,7 +13,6 @@ class light:
 		self.medium = 0 				# medium, 0=air 1=oil 2=water
 		self.finish = False
 		
-	
 class interface:
 	def __init__(self,x0,y0,z0,a,b,c,number):
 	# interface: ax + by + cz = CONST.
@@ -68,7 +56,7 @@ def refraction_or_reflection(light1,interface,case):
 	#print(o)
 	
 	sin_theta_out = sin_theta_in*(n1/n2)
-	cos_theta_out = math.sqrt(1-sin_theta_out**2)
+	cos_theta_out = sqrt(1-sin_theta_out**2)
 
 	#by Fresnel equations
 	R = ((n1*cos_theta_out-n2*cos_theta_in)/(n1*cos_theta_out+n2*cos_theta_in))**2  # reflectance(ratio of reflection)
@@ -110,11 +98,28 @@ def refraction_or_reflection(light1,interface,case):
 if __name__ =="__main__":
 	a = graph(width = 1200, align = 'left', ymin=0)
 		
-	
+##-------- VARIABLE DEFINITION --------##
+# INTERFACE
+	Xoil, Yoil, Zoil = 0.005,0,0				# a point on the air-oil interface
+	thetaD = 87 #deg							# dip angle of the normal vector of the air-oil plane
+	XoilD, YoilD, ZoilD = cos( thetaD* 2*pi/360),sin( thetaD* 2*pi/360),0
+
+	Xwater, Ywater, Zwater = 0.005,0,0			# a point on the oil-water interface
+	XwaterD,YwaterD,ZwaterD= 0,1,0				# normal vector ofthe oil-water interface
+
+	n = [1,1.5,1.33]							# refraction index (air, oil, water)
+# LIGHT
+	precise = 10000				# number of ligh beams
+	wavelength = [450,540,680] 	# blue, green, red
+	source_x_range = 0.001
+	source_y_range = 0
+	area_divide = 1000
+
+##-------- INTERNAL VARIABLES --------##
+	count = 0	
 	all_light_sum = []
 	
-
-	
+## GENERATE LIGHTS	
 	for k in range(len(wavelength)):
 		beams = []
 		re_beams = []
@@ -122,13 +127,14 @@ if __name__ =="__main__":
 		air_oil = interface(Xoil,Yoil,Zoil,XoilD,YoilD,ZoilD,0)
 		oil_water = interface(Xwater,Ywater,Zwater,XwaterD,YwaterD,ZwaterD,1)
 	
-		precise = 10000
+
 		for i in range(precise):
 			for j in range(1):
 			
-				beam = light(wavelength[k]*10**(-7),vec(-5+0.001/precise*i,5+0.001/precise*i,0/precise*j),vec(math.cos(pi/4),-math.sin(pi/4),0))
+				beam = light(wavelength[k]*1E-7,vec(-5 + source_x_range /precise*i, 5 + source_y_range/precise*i, 0/precise*j),vec(cos(pi/4),-sin(pi/4),0))
 				beams.append(beam)
-	
+
+## REFRACTION SIMULATION
 		for beam in beams:
 			refraction_or_reflection(beam,air_oil,0)
 		
@@ -141,35 +147,38 @@ if __name__ =="__main__":
 
 		for b in re_beams:
 			b.phase = (b.length/b.wl)%1*2*pi 
-			
+
+## MEAN_amplitude IN EACH SMALL AREA			
 		Xmax = beams[-1].source.x
 		Xmin = beams[0].source.x
-		Xarea = Xmax - Xmin
+		Xrange = Xmax - Xmin
 
-		light_sum = np.zeros((1000,2))	# light_sum [ area, # of light ]
+		light_sum = np.zeros((area_divide,2))	# light_sum [ area, # of light ]
 		for i in range(precise):
-				area_index = floor((beams[i].source.x-Xmin)/Xarea*1000)
-					#sum_amplitude = sqrt((Acos(a)+Bcos(b))**2 + (Asin(a)+Bsin(b))**2)
-					#sum_phase = atan([Asin(a)+Bsin(b)]/[Acos(a)+Bcos(b)])
-				# the last light has index 1000 hence ignore
-				if area_index < 1000: 
-					sum_amplitude = sqrt(\
-						(beams[i].amplitude*cos(beams[i].phase)+re_beams[i].amplitude*cos(re_beams[i].phase))**2\
-						+ (beams[i].amplitude*sin(beams[i].phase)+re_beams[i].amplitude*sin(re_beams[i].phase))**2)
+			area_index = floor((beams[i].source.x-Xmin)/Xrange*area_divide)
+		#sum_amplitude = sqrt((Acos(a)+Bcos(b))**2 + (Asin(a)+Bsin(b))**2)	
+		#sum_phase = atan([Asin(a)+Bsin(b)]/[Acos(a)+Bcos(b)])
+		# the last light has index 1000 hence ignore
+			if area_index < area_divide: 
+				sum_amplitude = sqrt(beams[i].amplitude**2 + re_beams[i].amplitude**2 + \
+					2*beams[i].amplitude*re_beams[i].amplitude*cos(beams[i].phase - re_beams[i].phase))  
 
-					light_sum[area_index,0] += sum_amplitude
-					light_sum[area_index,1] += 1
+				light_sum[area_index,0] += sum_amplitude
+				light_sum[area_index,1] += 1
 
-		for i in range(1000):
+		for i in range(area_divide):
 			if light_sum[i,1] !=0:
-				light_sum[i,0] = light_sum[i,0] / light_sum[i,1]
-		
+				light_sum[i,0] /= light_sum[i,1]
+
+## RESULT OF CERTAIN WAVELENGTH ADD TO LIST		
 		all_light_sum.append(light_sum)
 
+
+## SHOW ON SCREEN
 	dots = []
 	for i in range(len(all_light_sum[0])):
 
-		d = gdots(color = 125*vec(all_light_sum[2][i,0],all_light_sum[1][i,0],all_light_sum[0][i,0]))
+		d = gdots(color = 128*vec(all_light_sum[2][i,0],all_light_sum[1][i,0],all_light_sum[0][i,0]))
 		dots.append(d)
 
 	for i in range(len(dots)):
